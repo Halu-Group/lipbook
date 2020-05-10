0ddd4ed545a5df9ed7cd48db296a6fc04e3d93e7
Darkzzhelll <61936315+Darkzzhelll@users.noreply.github.com>
2020-05-10 22:46:54 +0700

    Update M_Pengembalian.php

--- application/models/M_Pengembalian.php
+++ application/models/M_Pengembalian.php
@@ -3,10 +3,11 @@ class M_Pengembalian extends CI_Model
 {
 	public function getAllDataPengembalian(){
 		//JOIN table buku dan Rak Buku
-		$this -> db -> table ('pengembalian');
-		$this -> db -> select ('Tgl_Kembali', 'Tgl_JatuhTempo', 'Denda','ID_Pinjam', 'pegawai.Nama');
-		$this -> db -> join('pegawai', 'pegawai.ID_Pegawai = pengembalian.ID_Pegawai');
-		//tampilin semua data baris
+		$this -> db -> table ('anggota');
+		$this -> db -> select ('peminjaman.ID_Pinjam', 'anggota.Nama','peminjaman.Tgl_pinjam', 'pengembalian.Tgl_JatuhTempo', 'pengembalian.denda');
+		$this -> db -> join('peminjaman', 'anggota.ID_Anggota = peminjaman.ID_Anggota');
+		$this -> db -> join('pengembalian', 'peminjaman.ID_Pinjam = pengembalian.ID_Pinjam');
+		//tampilin data 1 baris
 		$query = $this -> db -> get();
 		return $query -> result_array();
 	}
@@ -21,19 +22,42 @@ class M_Pengembalian extends CI_Model
 			"ID_Pinjam" => $this->input->post('ID_Pinjam', true),
 			"ID_Pegawai" => $this->input->post('ID_Pegawai', true)
 		];
-		$this->db->insert('pengembalian', $data);
+		$this->db->input('pengembalian', $data);
 	}
 
 	//Untuk Detail Buku (hanya satu baris yang tampil)
 	public function getDataPengembalianById($id)
 	{
 		$this -> db -> where ('ID_Kembali', $id);
-		//JOIN table buku dan Rak Buku
-		$this -> db -> table ('pengembalian');
-		$this -> db -> select ('Tgl_Kembali', 'Tgl_JatuhTempo', 'Denda','ID_Pinjam', 'pegawai.Nama');
-		$this -> db -> join('pegawai', 'pegawai.ID_Pegawai = pengembalian.ID_Pegawai');
-		//tampilin semua data baris
+
+		$this -> db -> table ('anggota');
+		$this -> db -> select ('peminjaman.ID_Pinjam', 'anggota.Nama','peminjaman.Tgl_pinjam', 'pengembalian.Tgl_JatuhTempo','pengembalian.Denda');
+		$this -> db -> join('peminjaman', 'anggota.ID_Anggota = peminjaman.ID_Anggota');
+		$this -> db -> join('pengembalian', 'peminjaman.ID_Pinjam = pengembalian.ID_Pinjam');
+		//tampilin data 1 baris
 		$query = $this -> db -> get();
 		return $query -> row_array();
+	}
+
+	public function getDataBukuBelumDikembalikan(){
+		$this -> db -> where ('pengembalian.ID_Pinjam => null');
+		$this -> db -> table ('anggota');
+		$this -> db -> select ('peminjaman.ID_Pinjam', 'anggota.Nama','peminjaman.Tgl_pinjam', 'pengembalian.Tgl_JatuhTempo', 'pengembalian.Denda');
+		$this -> db -> join('peminjaman', 'anggota.ID_Anggota = peminjaman.ID_Anggota');
+		$this -> db -> join('pengembalian', 'peminjaman.ID_Pinjam = pengembalian.ID_Pinjam');
+		return $this -> db -> get() -> result_array();
+	}
+
+	public function getDenda(){
+		$terlambat = $this -> db -> DATEDIFF ('pengembalian','Tgl_Kembali', 'Tgl_JatuhTempo');
+		if($terlambat > 0){
+			$denda = $terlambat * 2000;
+		}else {
+			$denda = 0;
+		}
+		$this -> db -> table ('anggota');
+		$this -> db -> select ('peminjaman.ID_Pinjam', 'anggota.Nama','peminjaman.Tgl_pinjam', 'pengembalian.Tgl_JatuhTempo', 'denda'.$denda);
+		$this -> db -> join('peminjaman', 'anggota.ID_Anggota = peminjaman.ID_Anggota');
+		$this -> db -> join('pengembalian', 'peminjaman.ID_Pinjam = pengembalian.ID_Pinjam');
 	}
 }
